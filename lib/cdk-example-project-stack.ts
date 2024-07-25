a5beb462266ee567a2eadedd9206ae94ad3c63a0
Sebastian Dirrigl <sebastian@dirrigl.org>
2024-07-25 18:21:20 +0200

    create mysql database

--- lib/cdk-example-project-stack.ts
+++ lib/cdk-example-project-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
 
 export class CdkExampleProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,8 +11,40 @@ export class CdkExampleProjectStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'CdkExampleProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new sqs.Queue(this, 'CdkExampleProjectQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300),
+    });
+
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2, // Default is all AZs in the region
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'isolated',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
+
+    const database = new rds.DatabaseInstance(this, 'MyDatabase', {
+      engine: rds.DatabaseInstanceEngine.mysql({
+        version: rds.MysqlEngineVersion.VER_8_0_37,
+      }),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE3,
+        ec2.InstanceSize.MICRO,
+      ),
+      vpc,
+      allocatedStorage: 20, // Minimum storage
+      maxAllocatedStorage: 100, // Allow for auto-scaling
+      multiAz: false, // Single-AZ for cost savings
+      publiclyAccessible: false, // For security
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      },
+      backupRetention: cdk.Duration.days(7), // Adjust as needed
+      deletionProtection: false, // Disable for development
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Adjust as needed
+    });
   }
 }
